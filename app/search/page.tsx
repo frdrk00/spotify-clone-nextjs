d7a9ae3730722849f78e46d7de5d25a7f8634f50
frdrk00 <masterkooo@gmail.com>
2023-06-03 15:53:25 +0200

    Favorites functionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -9,18 +9,22 @@ interface SearchProps {
   };
 }
 
+export const revalidate = 0;
+
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
-  
-  return <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
-    <Header className='from-bg-neutral-900'>
-    <div className='mb-2 flex flex-col gap-y-6'>
-<h1 className='text-white text-3xl font-semibold'>Search</h1>
+
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header className="from-bg-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">Search</h1>
+        </div>
+        <SearchInput />
+      </Header>
+      <SearchContent songs={songs} />
     </div>
-    <SearchInput />
-    </Header>
-    <SearchContent songs={songs} />
-  </div>
+  );
 };
 
 export default Search;
